cbe1e554a9a8914e13645a5a1623e03e684b576e
JetKnightAlpha <44746938+JetKnightAlpha@users.noreply.github.com>
2025-05-02 00:15:16 +0200

    Update TrafficLight.h

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -9,24 +9,20 @@ class TrafficLight {
 public:
     TrafficLight(Road* road, int position, int cycle);
 
-    // Update the light status based on the current time
     void update(double time);
 
-    // Get the status of the light (green or red)
     std::string getStatus() const;
 
-    // Check if the light is green
     bool isGreen() const;
 
-    // Get the position of the light on the road
     int getPosition() const;
 
 private:
-    Road* road;            // Road the light belongs to
-    int position;          // Position of the traffic light on the road
-    int cycle;             // Cycle time for the green/red light switch
-    bool green;            // Whether the light is green
-    double lastSwitchTime; // Last time the light changed status
+    Road* road;
+    int position;
+    int cycle;
+    bool green;
+    double lastSwitchTime;
 };
 
 #endif // TRAFFICLIGHT_H
