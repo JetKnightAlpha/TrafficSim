10d08791c362a0e8fe6003206338c6aa521bb4a7
JetKnightAlpha <44746938+JetKnightAlpha@users.noreply.github.com>
2025-05-02 00:14:14 +0200

    Update Simulation.h

--- src/Simulation.h
+++ src/Simulation.h
@@ -11,42 +11,34 @@ class VehicleGenerator;
 
 class Simulation {
 public:
-    // Constructor
     Simulation();
 
-    // Load simulation data from a file
     void loadFromFile(const std::string& filename);
 
-    // Run one simulation step
     void runStep();
 
-    // Run the simulation for a set number of steps
     void run();
 
-    // Output the current state of the simulation
     void outputState() const;
 
-    // Destructor
     ~Simulation();
 
-    // Getters for roads, vehicles, and traffic lights
     const std::vector<Road*>& getRoads() const;
     const std::vector<Vehicle*>& getVehicles() const;
     const std::vector<TrafficLight*>& getTrafficLights() const;
 
-    // Methods to add roads, vehicles, and traffic lights
     void addRoad(Road* road);
     void addVehicle(Vehicle* vehicle);
     void addTrafficLight(TrafficLight* light);
 
 private:
-    double currentTime;          // Current simulation time
-    int stepCounter;             // Counter for simulation steps
-    int vehicleCounter;          // Unique vehicle counter (for IDs)
-    std::vector<Road*> roads;    // List of roads
-    std::vector<Vehicle*> vehicles;    // List of vehicles
-    std::vector<TrafficLight*> trafficLights; // List of traffic lights
-    std::vector<VehicleGenerator*> generators; // Vehicle generators
+    double currentTime;
+    int stepCounter;
+    int vehicleCounter;
+    std::vector<Road*> roads;
+    std::vector<Vehicle*> vehicles;
+    std::vector<TrafficLight*> trafficLights;
+    std::vector<VehicleGenerator*> generators;
 };
 
 #endif // SIMULATION_H
